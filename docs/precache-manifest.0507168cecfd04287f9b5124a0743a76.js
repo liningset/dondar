1b50aa3062be6acdab8cf87400f62f6395f76d02
sajed <liningset2025@gmail.com>
2026-09-11 23:48:45 +0330

    fixed the issue of broken statics links

--- docs/precache-manifest.0507168cecfd04287f9b5124a0743a76.js
+++ docs/precache-manifest.0507168cecfd04287f9b5124a0743a76.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "b9acd5b8d31ed88a78fb",
-    "url": "/static/css/main.6440b0e0.chunk.css"
+    revision: "b9acd5b8d31ed88a78fb",
+    url: "/dondar/static/css/main.6440b0e0.chunk.css",
   },
   {
-    "revision": "b9acd5b8d31ed88a78fb",
-    "url": "/static/js/main.bcb2f36b.chunk.js"
+    revision: "b9acd5b8d31ed88a78fb",
+    url: "/dondar/static/js/main.bcb2f36b.chunk.js",
   },
   {
-    "revision": "345d2fe2806c5c9be170",
-    "url": "/static/js/runtime~main.680bbd6c.js"
+    revision: "345d2fe2806c5c9be170",
+    url: "/dondar/static/js/runtime~main.680bbd6c.js",
   },
   {
-    "revision": "5eaa4000c14c781abef1",
-    "url": "/static/js/2.a794a6ff.chunk.js"
+    revision: "5eaa4000c14c781abef1",
+    url: "/dondar/static/js/2.a794a6ff.chunk.js",
   },
   {
-    "revision": "15a50c1c2b2805fa91df",
-    "url": "/static/js/3.e92f44bb.chunk.js"
+    revision: "15a50c1c2b2805fa91df",
+    url: "/dondar/static/js/3.e92f44bb.chunk.js",
   },
   {
-    "revision": "2abeb837901aa58bc946997d5a4738f4",
-    "url": "/index.html"
-  }
+    revision: "2abeb837901aa58bc946997d5a4738f4",
+    url: "/index.html",
+  },
 ];
